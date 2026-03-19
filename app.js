4c37d6d77a5aec14c8548b65fdca5cca30c51080
Ben Fortin <benfort0801@gmail.com>
2026-03-18 22:01:30 -0400

    Revert "Remove tiktok domain swap because it now has native embeds"
    This reverts commit fe6c6057b9b1e6a8c981455bb96fa078f78b4ea2.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const DOMAIN_MAPPING = {
     'twitter.com': 'fxtwitter.com',
     'x.com': 'fixupx.com',
     'reddit.com': 'rxddit.com',
+    'tiktok.com': 'tnktok.com',
     'instagram.com': 'vxinstagram.com',
     'pixiv.net': 'phixiv.net',
     'bsky.app': 'bskyx.app',
